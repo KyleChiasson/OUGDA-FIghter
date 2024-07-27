f04bc94f2a06a7abd1f7a05770df97d218da5235
Dylan O'Donnell <78162298+BobTheTitan777@users.noreply.github.com>
2024-07-27 16:01:07 -0400

    stuffs

--- Assets/Scripts/Battler/HexGrid.cs
+++ Assets/Scripts/Battler/HexGrid.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class HexGrid : MonoBehaviour
 {
@@ -7,11 +8,13 @@ public class HexGrid : MonoBehaviour
     {
         public Vector2Int Position;
         public bool Terrain;
+        public GameObject Instance;
 
-        public Tile(int x, int y, bool T)
+        public Tile(int x, int y, bool T, GameObject I)
         {
             Position = new Vector2Int(x, y);
             Terrain = T;
+            Instance = I;
 
         }
     }
@@ -40,16 +43,19 @@ public class HexGrid : MonoBehaviour
             {
                 float R = Random.Range(0f, 1f);
                 
-                Grid.Add(new Tile(x, y, R <= TerrainChance));
+               
 
-                GameObject OBJ = Instantiate(HexBlock, new Vector3(x * (tileSize * Mathf.Sqrt(3)) + (y % 2 * (tileSize / 2 * Mathf.Sqrt(3))), 0, y * ((tileSize / 2) + tileSize)), Quaternion.Euler(-90, 0, 0), transform);
+                Vector3 LocalHex = new Vector3(x * (tileSize * Mathf.Sqrt(3)) + (y % 2 * (tileSize / 2 * Mathf.Sqrt(3))), 0, y * ((tileSize / 2) + tileSize));
+
+                GameObject OBJ = Instantiate(HexBlock, LocalHex, Quaternion.Euler(-90, 0, 0), transform);
                 if(R <= TerrainChance)
                 {
                     OBJ.GetComponent<MeshRenderer>().material.color = Color.gray;
 
-                    Instantiate(TerrainAsset, new Vector3(0, 0.5f, 0), Quaternion.Euler(0, 0, 0), OBJ.transform);
+                    Instantiate(TerrainAsset, new Vector3(0, 1f, 0) + LocalHex, Quaternion.Euler(-90, 0, 0), OBJ.transform);
                 }
-                
+
+                Grid.Add(new Tile(x, y, R <= TerrainChance, OBJ));
             }
         }
         
@@ -58,6 +64,28 @@ public class HexGrid : MonoBehaviour
     private void Start()
     {
         CreateGrid();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("left click");
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hitInfo))
+            {
+                Debug.Log("raycast");
+
+                Tile T = Grid.Where(i => i.Instance == hitInfo.collider.gameObject).First();
+                
+                if (T != null)
+                {
+                    Debug.Log("not null");
+                    
+                    T.Instance.GetComponent<MeshRenderer>().material.color = Color.blue;
+                }
+            }
+        }
     }
 
 }
